d0b5c744dce1cc2d2a55c889db03ecad7dbaa5ce
Jonathan Thompson <jathompson399@gmail.com>
2022-10-26 19:27:57 -0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import React, {useState} from "react"
-import Nav from './components/Nav'
-import About from './components/About'
-import logo from './logo.svg';
-import './App.css';
-
-function App(){
-    const [categories] = useState([
-        {
-            name: ""
-        }
-    ])
-
-    //const
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
-
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <ContactForm></ContactForm>
+      </main>
+    </div>
+  );
 }
 
 export default App;
