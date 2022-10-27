aa26ff63d9779d9c7eb8d32bb13b38e04a6c9f7e
Kal O <koestreicher4@gmail.com>
2022-10-26 19:25:13 -0500

    removed boilerplate return statement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
-import Nav from './components/Nav'
+// import Nav from './components/Nav'
 import About from './components/About'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App(){
@@ -12,29 +12,15 @@ function App(){
     ])
 
     //const
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-function App() {
+    return (
+      <div>
+        <main>
+          <About></About>
+        </main>
+      </div>
+    );
 
 }
+
 
 export default App;
