a442bb9c48c352bf394307e4e3c6471b7733940f
Sczygelski <avamariescz@gmail.com>
2022-10-28 08:59:42 -0700

    formatting components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react'
+
+function Nav(props){
+    const {
+        content = [],
+    } = props
+
+    return (
+        <header className="">
+            <nav>
+
+            </nav>
+            <ul>
+                
+            </ul>
+        </header>
+    )
+}
