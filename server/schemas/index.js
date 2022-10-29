1a9649b25a05e5cf9b3057eb8e4fa8cfe3a8d8c3
Susan Brown <brown684408@gmail.com>
2022-10-28 22:05:03 -0500

    setup resolvers and server.js in server directory

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,6 @@
 // this will collect "resolvers.js" and "typeDefs.js" and export them
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
