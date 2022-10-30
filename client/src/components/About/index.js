666b781d635c48ef17eaad182a8da6388341e475
Jonathan Thompson <jathompson399@gmail.com>
2022-10-30 15:14:39 -0500

    finished about styles

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,10 +4,25 @@ import coverImage from "../../assets/cover-image.jpg";
 function About() {
   return (
     <section className='about'>
-      <img src={coverImage} style={{ width: "100%" }} alt="cover" />
-      <div className="cover-info">
+      <div className="about-container">
+        <h2>About Us</h2>
         <p>
-          This is all the info we want to add about the site!
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Nunc sed augue lacus viverra vitae. Turpis nunc eget 
+          lorem dolor. Id nibh tortor id aliquet lectus proin. Vitae auctor 
+          eu augue ut lectus arcu bibendum. Libero nunc consequat interdum varius. 
+          Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. 
+          Neque laoreet suspendisse interdum consectetur. 
+          Pellentesque dignissim enim sit amet venenatis urna. 
+          Duis at tellus at urna condimentum mattis pellentesque. 
+          Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. 
+          Nam libero justo laoreet sit amet cursus sit amet dictum. 
+          Suspendisse in est ante in nibh mauris cursus. 
+          Pulvinar mattis nunc sed blandit libero. 
+          Sed viverra ipsum nunc aliquet. 
+          Varius quam quisque id diam vel quam. 
+          In mollis nunc sed id semper risus in hendrerit.
         </p>
       </div>
     </section>
