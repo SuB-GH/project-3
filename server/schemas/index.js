e13a0aa2016802773d51e090ac2241f9199e3490
Kal O <koestreicher4@gmail.com>
2022-10-29 20:50:48 -0500

    added models + schemas

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,5 +1,3 @@
-// this will collect "resolvers.js" and "typeDefs.js" and export them
-
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,20 +1,47 @@
-// import GraphQL tagged template function (which are like template literals)
 const { gql } = require('apollo-server-express');
 
-// typeDefs define the API endpoint, and also the exact data and parameters that are tied to that endpoint.
-// const typeDefs = gql`
-//   type Query {
-//     helloWisconsin: String
-//   }
-// `;
-
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Book {
-    id: ID
-    subject: String
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
-
-// export the typeDefs
 module.exports = typeDefs;
