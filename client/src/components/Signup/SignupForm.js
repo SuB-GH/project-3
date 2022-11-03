ea93e68497128775c61dc13f3b6fa1bc4e2f9d0c
Jonathan Thompson <jathompson399@gmail.com>
2022-11-02 22:26:45 -0500

    login

--- client/src/components/Signup/SignupForm.js
+++ client/src/components/Signup/SignupForm.js
@@ -1,85 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-  const [addUser, { error }] = useMutation(ADD_USER);
+const SignupForm = () => {
+  // set initial form state
+   const [userFormData, setUserFormData] = useState({ 
+     username: '', 
+     email: '', 
+     password: '' 
+   });
+ 
+   // set state for form validation
+   const [validated] = useState(false);
+   // set state for alert
+   const [showAlert, setShowAlert] = useState(false);
+ 
+   const [addUser, { error, data }] = useMutation(ADD_USER);
+ 
+   useEffect(() => {
+     if (error) {
+       setShowAlert(true);
+     } else {
+       setShowAlert(false);
+     }
+   }, [error]);
+ 
+ 
+   const handleFormSubmit = async (event) => {
+     event.preventDefault();
+ 
+     // check if form has everything (as per react-bootstrap docs)
+     const form = event.currentTarget;
+     if (form.checkValidity() === false) {
+       event.preventDefault();
+       event.stopPropagation();
+     }
+ 
+     try {
+       const { data } = await addUser({
+         variables: { ...userFormData },
+       });
+ 
+       Auth.login(data.addUser.token);
+     } catch (err) {
+       console.error(err);
+     }
+ 
+     setUserFormData({
+       username: '',
+       email: '',
+       password: '',
+     });
+   };
+   const [formState, setFormState] = useState({ email: '', password: ''});
+ 
+   const [errorMessage, setErrorMessage] = useState('');
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+ 
+   const handleChange = (e) => {
+     if (e.target.name === 'email') {
+       const isValid = validateEmail(e.target.value);
+       if (!isValid) {
+         setErrorMessage('Your email is invalid.');
+       } else {
+         setErrorMessage('');
+       }
+     } else {
+       if (!e.target.value.length) {
+         setErrorMessage(`A ${e.target.name} is required.`);
+       } else {
+         setErrorMessage('');
+       }
+     }
+     if (!errorMessage) {
+       setFormState({ ...formState, [e.target.name]: e.target.value });
+       console.log('Handle Form', formState);
+     }
+   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit" onSubmit={handleFormSubmit}>
-                Submit
-              </button>
-            </form>
-
-            {error && <div>Signup failed</div>}
-          </div>
+    <form onSubmit={handleFormSubmit}>
+      <div className='login-card'>
+        <h3>Sign Up Here:</h3>
+        <div className='login-section'>
+          <label htmlFor="email">Email address:</label>
+          <input type="login" name="Email" defaultValue={formState.email} onBlur={handleChange}/>
         </div>
+        <div className='login-section'>
+          <label htmlFor="password">Password:</label>
+          <input type="login" name="password" defaultValue={formState.password} onBlur={handleChange}/>
+        </div>
+        {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+        <button data-testid="button" type="submit" className='submit'>Submit</button>
       </div>
-    </main>
+    </form>
   );
 };
 
