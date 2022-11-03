c4abfa8749a3e0c2c7677d11e6b25d84462c2ed8
Kal O <koestreicher4@gmail.com>
2022-11-02 20:54:42 -0500

    commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,28 +36,8 @@ const resolvers = {
 
       return { token, user };
     },
-    saveBook: async (parent, { newBook }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { savedBooks: newBook }},
-          { new: true }
-        );
-        return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId }}},
-          { new: true }
-        );
-        return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
+
+
   },
 };
 
